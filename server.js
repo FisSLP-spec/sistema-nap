5430fbd1dce1051a1241df31d2d8cf72e3a01939
FisSLP-spec <claum35@gmail.com>
2026-09-07 01:00:36 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ db.serialize(() => {
     stmt.finalize();
 });
 app.use(express.static(path.join(__dirname))); 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index1.html')); });
 // 3. Crear la ruta (API) para verificar los folios
 app.get('/api/verificar', (req, res) => {res.sendFile(path.join(__dirname, 'index1.html')); 
     // .trim() limpia espacios en blanco si el usuario los escribe por error
